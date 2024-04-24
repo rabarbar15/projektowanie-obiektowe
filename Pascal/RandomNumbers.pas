3099ba4e9873e69487ca89ba72dc5eb15d86997e
Kamil Piskorz <kamilp939@gmail.com>
2024-04-24 15:04:26 +0200

    Procedura do losowania liczb

--- Pascal/RandomNumbers.pas
+++ Pascal/RandomNumbers.pas
@@ -1,12 +1,10 @@
 program SimpleForLoop;
 
 
-procedure GenerateRandomNumbers();
+procedure GenerateRandomNumbers(var arr: array of integer);
 var
   i, randomNumber: integer;
-  arr: array Of integer;
 begin
-  SetLength(arr, 50);
 
   for i := 1 to 50 do
   begin
@@ -16,7 +14,34 @@ begin
   end;
 end;
 
+procedure Sorting(var arr: array of integer);
+var
+  i, j, temp: integer;
 begin
+  for i := Low(arr) to High(arr) - 1 do
+  begin
+    for j := Low(arr) to High(arr) - 1 do
+    begin
+      if arr[j] > arr[j + 1] then
+      begin
+        temp := arr[j];
+        arr[j] := arr[j + 1];
+        arr[j + 1] := temp;
+      end;
+    end;
+  end;
+end;
+
+var
+  arr: array of integer;
+  i: integer;
+begin
+    SetLength(arr, 50);
     Randomize;
-    GenerateRandomNumbers();
+    GenerateRandomNumbers(arr);
+    WriteLn('Po sortowaniu: ');
+    Sorting(arr);
+
+    for i := Low(arr) to High(arr) do
+        WriteLn(arr[i], ' ');
 end.
