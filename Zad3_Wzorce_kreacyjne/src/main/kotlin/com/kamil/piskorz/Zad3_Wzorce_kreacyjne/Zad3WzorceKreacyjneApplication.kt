c5940d9901393a07d19a54ebdb1e742b356ed238
Kamil Piskorz <kamilp939@gmail.com>
2024-05-01 22:09:42 +0200

    Initial commit

---
+++ Zad3_Wzorce_kreacyjne/src/main/kotlin/com/kamil/piskorz/Zad3_Wzorce_kreacyjne/Zad3WzorceKreacyjneApplication.kt
@@ -0,0 +1,11 @@
+package com.kamil.piskorz.Zad3_Wzorce_kreacyjne
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Zad3WzorceKreacyjneApplication
+
+fun main(args: Array<String>) {
+	runApplication<Zad3WzorceKreacyjneApplication>(*args)
+}
---
+++ Zad3_Wzorce_kreacyjne/src/test/kotlin/com/kamil/piskorz/Zad3_Wzorce_kreacyjne/Zad3WzorceKreacyjneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kamil.piskorz.Zad3_Wzorce_kreacyjne
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Zad3WzorceKreacyjneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
